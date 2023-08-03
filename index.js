450c76ca2c196009fbfddcb35a296d4ba451470d
tushar-c23 <tusharc.cse@gmail.com>
2023-08-03 23:52:54 +0530

    Basic organisation of dir struct and sequelize connection successfull

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+
+const userRoutes = require('./routes/user');
+
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/uk_coop_be');
+
+//DB Connection test
+async function dbTest() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+dbTest();
+
+const app = express();
+
+// app.use('/user', userRoutes);
+// app.use('/application', applicationRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
